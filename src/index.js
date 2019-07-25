33510404aa3749a2b6adb002a0296f640136a140
sarahkirby <sk.irby@icloud.com>
2019-07-25 19:18:10 +0100

    Add node-sass for styles, setup base styles
    and fonts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-import './index.css';
+import './index.scss';
 import ScrollToTop from 'components/Helpers/ScrollToTop';
 import App from 'pages/app';
 import * as serviceWorker from './serviceWorker';
