a2fc712751d92a4b53693b46ae354ae727d872b0
sarahkirby <sk.irby@icloud.com>
2019-07-29 15:35:53 +0100

    Style ShowSingle -> ShowInfo component
    Update black color variables, link styles

--- src/components/ShowSingle/Cast.js
+++ src/components/ShowSingle/Cast.js
@@ -2,40 +2,38 @@ import React from 'react';
 
  const Cast = (props) => (
   <div className="cast">
-    <h2>Starring</h2>
+    {props.cast.length > 0 &&
+      <div>
+        <h2>Starring</h2>
 
-    {!props.cast.length &&
-      <p className="text--secondary">
-        Whoops, we can't find the cast!
-      </p>
+        <ul className="cast__list">
+          {props.cast.map((person, index) =>
+            <li
+              key={index}
+              className="cast__item"
+            >
+              <div className="image-circle__wrapper">
+                <img
+                  src={person.cast.person.image.medium}
+                  alt={person.cast.person.name}
+                  className="cast__image image-circle__image"
+                />
+              </div>
+
+              <div className="cast__info">
+                <h4 className="cast__actor">
+                  {person.cast.person.name}
+                </h4>
+
+                <h5 className="cast__character font--reg">
+                  {person.cast.character.name.split(" ")[0]}
+                </h5>
+              </div>
+            </li>
+          )}
+        </ul>
+      </div>
     }
-
-    <ul className="cast__list">
-      {props.cast.map((person, index) =>
-        <li
-          key={index}
-          className="cast__item"
-        >
-          <div className="image-circle__wrapper">
-            <img
-              src={person.cast.person.image.medium}
-              alt={person.cast.person.name}
-              className="cast__image image-circle__image"
-            />
-          </div>
-
-          <div className="cast__info">
-            <h4 className="cast__actor">
-              {person.cast.person.name}
-            </h4>
-
-            <h5 className="cast__character font--reg">
-              {person.cast.character.name.split(" ")[0]}
-            </h5>
-          </div>
-        </li>
-      )}
-    </ul>
   </div>
  );
 
--- src/components/ShowSingle/ShowInfo.js
+++ src/components/ShowSingle/ShowInfo.js
@@ -1,35 +1,55 @@
 import React from 'react';
 
- const ShowInfo = (props) => (
+ const ShowInfo = (props) => console.log(props) || (
   <div className="showinfo">
     <h2>Show Info</h2>
 
-    {props.network &&
-      <a
-        href={props.officialSite}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-      <p>{props.network.name}</p>
-      </a>
-    }
+    <ul className="showinfo__list">
+      {props.network &&
+        <li className="showinfo_item">
+          <h4 className="showinfo__title">Streamed on</h4>
+          <span className="showinfo__content">
+            <a
+              href={props.officialSite}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="link"
+            >
+              {props.network.name}
+            </a>
+          </span>
+        </li>
+      }
 
-    {props.schedule &&
-      <div>
-        {props.schedule.days.map((day, index) =>
-          <div key={index}>
-            <ul>
-              <li>{day}</li>
-            </ul>
-          </div>
-        )}
-      </div>
-    }
-    
-    <p>{props.status}</p>
+      {props.schedule &&
+        <li className="showinfo_item">
+          <h4 className="showinfo__title">Schedule</h4>
+          <span className="showinfo__content">
+            {props.schedule.days.length > 1
+              ? props.schedule.days.join(', ')
+              : `${props.schedule.days}s`
+            }
+          </span>
+        </li>
+      }
 
-    <p>{props.genres}</p>
+      {props.status &&
+        <li className="showinfo_item">
+          <h4 className="showinfo__title">Status</h4>
+          <span className="showinfo__content">{props.status}</span>
+        </li>
+      }
+
+      {props.genres && props.genres.length > 0 &&
+        <li className="showinfo_item">
+          <h4 className="showinfo__title">Genres</h4>
+          <span className="showinfo__content">
+            {props.genres.join(', ')}
+          </span>
+        </li>
+      }
+    </ul>
   </div>
- );
+);
 
 export default ShowInfo;
