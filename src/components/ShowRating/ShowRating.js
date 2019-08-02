f46f2a418aa16a8ce321d1c530a75cdc54247fa9
sarahkirby <sk.irby@icloud.com>
2019-08-02 10:52:51 +0100

    Refactor ShowRating component - add for loop
    to be more DRY. Remove unused Google fonts

--- src/components/ShowRating/ShowRating.js
+++ src/components/ShowRating/ShowRating.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
- const ShowRating = (props) => {
+const ShowRating = (props) => {
   const outOfFive = props.rating / 2;
   const ratingRounded = Math.round(outOfFive);
+  const stars = [];
+
+  for (let i = 1; i <= 5; i++) {
+    stars.push(
+      <i
+        key={i}
+        className={`showrating__star fa-star ${ratingRounded >= i ? `fas` : `far o-40`}`}
+      />
+    );
+  }
   return (
     <div className={`showrating ${props.color ? props.color : ''}`}>
-      <i className={`showrating__star fa-star ${ratingRounded >= 1 ? `fas` : `far o-40`}`} />
-      <i className={`showrating__star fa-star ${ratingRounded >= 2 ? `fas` : `far o-40`}`} />
-      <i className={`showrating__star fa-star ${ratingRounded >= 3 ? `fas` : `far o-40`}`} />
-      <i className={`showrating__star fa-star ${ratingRounded >= 4 ? `fas` : `far o-40`}`} />
-      <i className={`showrating__star fa-star ${ratingRounded >= 5 ? `fas` : `far o-40`}`} />
-
+      {stars}
       {props.displayNumber &&
         <span className="showrating__text">
           {outOfFive}/5
@@ -18,6 +23,6 @@ import React from 'react';
       }
     </div>
   );
- }
+}
 
 export default ShowRating;
